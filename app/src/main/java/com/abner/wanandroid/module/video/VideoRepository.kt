552cc39c18f296b170b9c9416facad171e9b77d0
zhangdt <274496856@qq.com>
2019-03-06 17:58:31 +0800

    加2个类  晚上回家补充

---
+++ app/src/main/java/com/abner/wanandroid/module/video/VideoRepository.kt
@@ -0,0 +1,10 @@
+package com.abner.wanandroid.module.video
+
+/**
+ *
+ *
+ * @author zhangduntai
+ * @date 2019/3/6
+ */
+class VideoRepository {
+}
---
+++ app/src/main/java/com/abner/wanandroid/module/video/api/EyepetizerApi.kt
@@ -0,0 +1,11 @@
+package com.abner.wanandroid.module.video.api
+
+/**
+ *
+ *
+ * @author zhangduntai
+ * @date 2019/3/6
+ */
+interface EyepetizerApi {
+
+}
